524622d95ec29dd398feb878a5e77c6d38ddde24
Eddie Bugg <zarhim@gmail.com>
2019-06-19 22:22:02 -0500

    Stole most of this from dtrodger/ad for more stuff

---
+++ app/extensions/__init__.py
@@ -0,0 +1,16 @@
+from app.extensions.log import configure_logging
+from app.extensions.rest import rest_api, http_status_codes
+
+def initialize_extensions(app):
+
+    # Logging
+    configure_logging(app)
+
+    # Flask-Restful
+    endpoint_registries = [register_zelos_endpoints]
+    rest_api.init_app(app, endpoint_registries, http_status_codes)
+
+    if app.config['ENV'] == 'production' and app.name != 'test':
+        pass
+
+    return app
