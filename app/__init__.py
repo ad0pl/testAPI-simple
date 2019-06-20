b2ed4f77dfce28b868338f2b452db010d346fc78
Eddie Bugg <zarhim@gmail.com>
2019-06-19 22:21:13 -0500

    Stole most of this from dtrodger/ad

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ from app.config import (
     Config
 )
 
+from app.extensions import initialize_extensions
+
 def create_app(config='develop', app_name=Config.PROJECT):
     app = Flask(app_name, static_folder=None)
     config_obj = config_options(config)
     app.config.from_object(config_obj)
-    #initialize_extensions(app)
+    initialize_extensions(app)
     #register_error_handlers(app)
 
     return app
